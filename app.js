b4fda4d5ed75d4bd26f395f1b92979ab1ed50d28
Yash Garg <yash06garg@gmail.com>
2023-10-04 18:16:33 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/public/html/data.html");
+});
+
+app.get("/data",function(req,res){
+  res.sendFile(__dirname + "/public/html/data.html");
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function(){
+   console.log("server is up and running on port 5000!!");
+});
